2f0ef96d4694871698137e03d4ac1253d83c8b13
Sittisorn <sittisorn.k@mde.co.th>
2021-08-15 02:52:19 +0700

    add: all book component

---
+++ components/allBook.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-responsive class="text-center pa-2">
+    <v-lazy
+      v-model="isActive"
+      :options="{
+        threshold: 0.5,
+      }"
+      transition="fade-transition"
+    >
+      <v-item-group>
+        <v-container>
+          <v-row>
+            <v-col
+              v-for="(data, index) in bookList"
+              :key="index"
+              cols="3"
+              md="3"
+            >
+              <v-item>
+                <bookCard
+                  :id="data.id"
+                  :image="data.imagePath"
+                  :ratingScore="data.averageReviewScore"
+                  :ratingCount="data.reviewCount"
+                  :name="data.name"
+                  :price="data.price"
+                />
+              </v-item>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-item-group>
+    </v-lazy>
+  </v-responsive>
+</template>
+<script>
+import bookCard from "../components/bookCard.vue";
+export default {
+  components: { bookCard },
+  data() {
+    return {
+      isActive: false,
+      bookList: {
+        id: "",
+        image: "",
+        ratingScore: "",
+        ratingCount: "",
+        name: "",
+        price: "",
+      },
+    };
+  },
+  async fetch() {
+    const payload = {
+      params: {
+        // "tag": this.bookTag,
+      },
+    };
+
+    const resonse = await this.$axios.get(`/api/books`, payload);
+    this.bookList = resonse.data["hydra:member"];
+    // console.log(this.bookList);
+  },
+};
+</script>
