2413090955477146ecbfaf2c47b796d56abd3d7c
Sittisorn <sittisorn.k@mde.co.th>
2021-08-14 20:53:57 +0700

    add: bookCard lazyload book cover

--- components/bookCard.vue
+++ components/bookCard.vue
@@ -1,46 +1,46 @@
 <template>
   <v-hover v-slot="{ hover }">
-      <v-card
-        :loading="loading"
-        class="mx-2"
-        :elevation="hover ? 12 : 2"
-        :class="{ 'on-hover': hover }"
-        @click="goBook(id)" 
-      >
-        <template slot="progress">
-          <v-progress-linear
-            color="deep-purple"
-            height="10"
-            indeterminate
-          ></v-progress-linear>
-        </template>
-        <center>
-          <v-img contain max-width="150" max-height="200" :src="image"></v-img>
-        </center>
-        <v-card-text v-if="ratingScore">
-          <v-row class="mx-0">
-            <v-rating
-              :value="ratingScore"
-              color="amber"
-              dense
-              half-increments
-              readonly
-              size="14"
-            ></v-rating>
-            <div class="grey--text ms-4" v-if="ratingCount">
-              Reviews ({{ ratingCount }})
-            </div>
-          </v-row>
-        </v-card-text>
-        <v-card-title>{{ name }}</v-card-title>
-        <v-card-text align="left">
-          <div class="my-4 text-subtitle-1" v-if="price">฿ {{ price }}</div>
-        </v-card-text>
-      </v-card>
-    
+    <v-card
+      :loading="loading"
+      class="mx-2"
+      :elevation="hover ? 12 : 2"
+      :class="{ 'on-hover': hover }"
+      @click="goBook(id)"
+    >
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="10"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+      <center>
+        <v-img lazy-src="loading-book.jpg" contain max-width="150" max-height="200" :src="image"></v-img>
+      </center>
+      <v-card-text v-if="ratingScore">
+        <v-row class="mx-0">
+          <v-rating
+            :value="ratingScore"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+          <div class="grey--text ms-4" v-if="ratingCount">
+            Reviews ({{ ratingCount }})
+          </div>
+        </v-row>
+      </v-card-text>
+      <v-card-title>{{ name }}</v-card-title>
+      <v-card-text align="left">
+        <div class="my-4 text-subtitle-1" v-if="price">฿ {{ price }}</div>
+      </v-card-text>
+    </v-card>
   </v-hover>
 </template>
 <script>
+
 export default {
   props: {
     id: {
@@ -67,9 +67,9 @@ export default {
   }),
 
   methods: {
-    goBook(id){
-      this.$router.push('/book/'+id)
-    }
+    goBook(id) {
+      this.$router.push("/book/" + id);
+    },
   },
 };
 </script>
